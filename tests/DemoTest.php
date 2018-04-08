29afc247cd52d0bb5472e72954e34c13d5959754
xshaitt <773999579@qq.com>
2018-04-08 13:15:57 +0800

    添加测试文件

---
+++ tests/DemoTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xshaitt
+ * Date: 2018/4/4
+ * Time: 下午3:56
+ */
+//require __DIR__.'/../vendor/autoload.php';
+//use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testPass()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testFail()
+    {
+        $this->assertFalse(false);
+    }
+}
